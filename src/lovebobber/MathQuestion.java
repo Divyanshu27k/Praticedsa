b9cb008c7f756c9cef03e3e71cc319b7b630ab63
Divyanshu <divyanshusingh0081@gmail.com>
2026-06-29 16:12:42 +0530

    Math question

---
+++ src/lovebobber/MathQuestion.java
@@ -0,0 +1,12 @@
+package lovebobber;
+
+public class MathQuestion {
+    public static void main(String[] args) {
+        int a=10;
+        int b=23;
+        System.out.println("this is number :"+a+ "+" +b);
+        int sum=a+b;
+        System.out.println(sum);
+        System.out.println("This is final answer : "+ sum);
+    }
+}
