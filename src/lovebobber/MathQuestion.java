c1fdde6ff7d74c98fade1420ba9b91b97b966d66
Divyanshu <divyanshusingh0081@gmail.com>
2026-06-30 12:41:01 +0530

    Sum math questions

--- src/lovebobber/MathQuestion.java
+++ src/lovebobber/MathQuestion.java
@@ -1,12 +1,48 @@
 package lovebobber;
 
+import java.util.Scanner;
+
 public class MathQuestion {
     public static void main(String[] args) {
-        int a=10;
-        int b=23;
-        System.out.println("this is number :"+a+ "+" +b);
-        int sum=a+b;
-        System.out.println(sum);
-        System.out.println("This is final answer : "+ sum);
+
+        //addition of two numbers
+//        int a=10;
+//        int b=23;
+//        System.out.println("this is number :"+a+ "*" +b);
+//        int sum=a+b;
+//        System.out.println(sum);
+//        System.out.println("This is final answer : "+ sum);
+
+        //multiple by two numbers
+//        int a=27234;
+//        int b=14234;
+//        System.out.println("This is number :"+a+ "*" +b);
+//        int multiple=a*b;
+//        System.out.println("This is final answer : "+ multiple);
+
+        //With using Scanner class add
+//        Scanner sc=new Scanner(System.in);
+//
+//        System.out.println("Enter the first number : ");
+//        int a=sc.nextInt();
+//
+//        System.out.println("Enter the Second nnumber : ");
+//        int b=sc.nextInt();
+//
+//        System.out.println("Sum = " +(a+b));
+//        sc.close();
+
+        //multiple
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter the first number : ");
+        int a=sc.nextInt();
+
+        System.out.println("Enter the Second nnumber : ");
+        int b=sc.nextInt();
+
+        System.out.println("Sum = " +(a*b));
+        sc.close();
+
     }
 }
