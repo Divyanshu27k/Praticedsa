7d5a27a34d31cf03fea216b0f9f8c1249930c777
Divyanshu <divyanshusingh0081@gmail.com>
2026-07-03 06:03:07 +0530

    oops operationtesting

--- src/lovebobber/OopsOperation.java
+++ src/lovebobber/OopsOperation.java
@@ -1,7 +1,22 @@
 package lovebobber;
 
 public class OopsOperation {
+    int age;
+    String student_name;
+    String classs;
+    String roll_number;
+    String father_name;
+    String address;
+    String mobile_number;
+    String school_name;
+
     public static void main(String[] args) {
+
+        //Q1.How will you model a program having 100s of students, and each students have their own properties, behaviours and something to hide?
+
+
+
+
 
     }
 }
