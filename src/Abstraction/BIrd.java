f2a981baa54b3dbd196cddc351ce99a5f9f0b0a5
Divyanshu Kumar <divyanshusingh0081@gmail.com>
2026-07-12 23:14:45 +0530

    abstractionn code

--- src/Abstraction/BIrd.java
+++ src/Abstraction/BIrd.java
@@ -1,67 +1,67 @@
-package Abstraction;
-
-public interface BIrd {
-    void fly();
-
-    void eat();
-
-}
-class STparrow implements BIrd {
-
-
-    @Override
-    public void fly() {
-        System.out.println("Sparrow is fying");
-    }
-
-    @Override
-    public void eat() {
-        System.out.println("Sparrow is eat");
-
-    }
-}
-class TParrot implements BIrd {
-
-
-    @Override
-    public void fly() {
-
-    }
-
-    @Override
-    public void eat() {
-
-    }
-}
-
-
-public class Main {
-    public static void doBirds(Bird b) {
-        b.fly();
-        b.eat();
-        b.fly();
-        b.eat();
-        b.fly();
-        b.eat();
-        b.fly();
-        b.eat();
-        b.fly();
-        b.eat();
-
-    }
-
-    public static void main(String[] args) {
-        doBirds(new Sparrow());
-        doBirds(new Parrot());
-    }
-}
-
-//    public static void main(String[] args) {
-//        Bird bd =new Sparrow();
-//        bd.eat();
-//        bd.fly();
-//        bd= new Parrot();
-//        bd.eat();
-//        bd.fly();
+//package Abstraction;
+//
+//public interface BIrd {
+//    void fly();
+//
+//    void eat();
+//
+//}
+//class STparrow implements BIrd {
+//
+//
+//    @Override
+//    public void fly() {
+//        System.out.println("Sparrow is fying");
 //    }
-
+//
+//    @Override
+//    public void eat() {
+//        System.out.println("Sparrow is eat");
+//
+//    }
+//}
+//class TParrot implements BIrd {
+//
+//
+//    @Override
+//    public void fly() {
+//
+//    }
+//
+//    @Override
+//    public void eat() {
+//
+//    }
+//}
+//
+//
+//public class Main {
+//    public static void doBirds(Bird b) {
+//        b.fly();
+//        b.eat();
+//        b.fly();
+//        b.eat();
+//        b.fly();
+//        b.eat();
+//        b.fly();
+//        b.eat();
+//        b.fly();
+//        b.eat();
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        doBirds(new Sparrow());
+//        doBirds(new Parrot());
+//    }
+//}
+//
+////    public static void main(String[] args) {
+////        Bird bd =new Sparrow();
+////        bd.eat();
+////        bd.fly();
+////        bd= new Parrot();
+////        bd.eat();
+////        bd.fly();
+////    }
+//
