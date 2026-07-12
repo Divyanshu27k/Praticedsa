ed452893d38c38c8dd4f41b58a5f154d297f3a68
Divyanshu Kumar <divyanshusingh0081@gmail.com>
2026-07-12 23:00:52 +0530

    abstractionn code

--- src/Abstraction/Main.java
+++ src/Abstraction/Main.java
@@ -19,13 +19,40 @@ class Sparrow extends Bird {
         System.out.println("sparrow eating");
     }
 }
+class Parrot extends Bird {
 
+    @Override
+    void fly() {
+        System.out.println("Parrot fly");
+
+    }
+
+    @Override
+    void eat() {
+        System.out.println("Parrot eating");
+    }
+}
 
 
 public class Main {
-    public static void main(String[] args) {
-        Bird bd =new Sparrow();
-        bd.eat();
-        bd.fly();
+    public static void doBirds(Bird b){
+        b.fly();
+        b.eat();
     }
+
+    public static void main(String[] args) {
+        doBirds(new Sparrow());
+        doBirds(new Parrot());
+    }
+
+
+//    public static void main(String[] args) {
+//        Bird bd =new Sparrow();
+//        bd.eat();
+//        bd.fly();
+//        bd= new Parrot();
+//        bd.eat();
+//        bd.fly();
+//    }
+
 }
