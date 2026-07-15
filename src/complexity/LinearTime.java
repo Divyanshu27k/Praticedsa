60d451533d31eadd33191a6513634080e11c6e17
Divyanshu Kumar <divyanshusingh0081@gmail.com>
2026-07-15 23:14:44 +0530

    Testing

---
+++ src/complexity/LinearTime.java
@@ -0,0 +1,13 @@
+package complexity;
+
+public class LinearTime {
+    //O(n) - Linear Time
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
