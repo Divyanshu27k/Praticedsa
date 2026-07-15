c7d91fb856df8d7da11a1ee7303d6f1c697f291f
Divyanshu Kumar <divyanshusingh0081@gmail.com>
2026-07-15 23:08:06 +0530

    Testing

---
+++ src/complexity/SpaceComp.java
@@ -0,0 +1,6 @@
+package complexity;
+
+public class SpaceComp {
+
+    //duration on running code when acure the space in memeroy are kanow as sapcecomp.
+}
---
+++ src/complexity/TimeComp.java
@@ -0,0 +1,6 @@
+package complexity;
+
+public class TimeComp {
+
+    //this one duration on taken time in code are known as timeComp.
+}
