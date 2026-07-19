d131d1f62636b55032e822058e275d1a34f9d120
Divyanshu Kumar <divyanshusingh0081@gmail.com>
2026-07-19 23:06:59 +0530

    collectionf prc

--- src/colllection/CollectionP.java
+++ src/colllection/CollectionP.java
@@ -3,13 +3,19 @@ package colllection;
 import collection.Arraylist;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class CollectionP {
     public static void main(String[] args) {
 
-        Arraylist<Integer> arr=new Arraylist();
-        List<Integer> list=new ArrayList<>();
+        ArrayList<Integer>list =new ArrayList<>();
+        list.add(12);
+        list.add(83);
+        list.add(13);
+        list.add(23);
+//        List<Integer> list=new ArrayList<>();
+//        Collection<Integer> col=new ArrayList<>();
 
     }
 
