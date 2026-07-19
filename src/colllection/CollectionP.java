2bcb98525400f647773d761393630170e14632c1
Divyanshu Kumar <divyanshusingh0081@gmail.com>
2026-07-19 22:41:22 +0530

    collectionf prc

---
+++ src/colllection/CollectionP.java
@@ -0,0 +1,16 @@
+package colllection;
+
+import collection.Arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionP {
+    public static void main(String[] args) {
+
+        Arraylist<Integer> arr=new Arraylist();
+        List<Integer> list=new ArrayList<>();
+
+    }
+
+}
