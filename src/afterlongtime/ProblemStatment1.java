2811ed1b8c53eabe054834929d4f47e9f9578d90
Divyanshu Kumar <divyanshusingh0081@gmail.com>
2026-08-07 00:28:29 +0530

    try to understand String and int

---
+++ src/afterlongtime/ProblemStatment1.java
@@ -0,0 +1,21 @@
+package afterlongtime;
+import java.util.*;
+//Problem statement
+//Take the person's name and age as input and print out the name and age as specified in the output format.
+//
+//Detailed explanation ( Input/output format, Notes, Images )
+//Sample Input 1:
+//Ali
+//30
+//Sample Output 1:
+//The name of the person is Ali and the age is 30.
+//Explanation of Sample Input 1:
+//The input name is Ali and the input age is 30 which is printed in the specified format.
+public class ProblemStatment1 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String name=sc.nextLine();
+        int age=sc.nextInt();
+        System.out.println("The name of the person is " +name+ " and the input age is "+age);
+    }
+}
