ac36a53b119de6bcc076d5dea93fc10e476637a0
Divyanshu Kumar <divyanshusingh0081@gmail.com>
2026-08-07 17:48:59 +0530

    ProblemStatment Question

---
+++ src/afterlongtime/ProblemStatment4.java
@@ -0,0 +1,10 @@
+package afterlongtime;
+
+import java.util.Scanner;
+
+public class ProblemStatment4 {
+    public static void main(String[] args) {
+        //Scanner sc =new Scanner(System.in);
+
+    }
+}
