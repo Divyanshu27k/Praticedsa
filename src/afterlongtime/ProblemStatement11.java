32c212c185beb8a5c16ba7af93d670a2c0bc785e
Divyanshu Kumar <divyanshusingh0081@gmail.com>
2026-08-09 23:21:38 +0530

    ProblemStatement Question

---
+++ src/afterlongtime/ProblemStatement11.java
@@ -0,0 +1,31 @@
+package afterlongtime;
+
+import java.util.Scanner;
+
+public class ProblemStatement11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int evenSum = 0;
+        int oddSum = 0;
+
+        while (n > 0) {
+
+            int digit = n % 10;
+
+            if (digit % 2 == 0) {
+                evenSum += digit;
+            } else {
+                oddSum += digit;
+            }
+
+            n = n / 10;
+        }
+
+        System.out.println(evenSum + " " + oddSum);
+
+        sc.close();
+    }
+}
