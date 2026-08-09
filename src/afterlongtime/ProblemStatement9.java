0697daff70f0453ee81c69a0f1521bfc24b59293
Divyanshu Kumar <divyanshusingh0081@gmail.com>
2026-08-09 22:57:10 +0530

    ProblemStatment Question

---
+++ src/afterlongtime/ProblemStatement9.java
@@ -0,0 +1,11 @@
+package afterlongtime;
+
+public class ProblemStatement9 {
+    public static long evenSumTillN(int n) {
+        long sum=0;
+        for (int i=2;i<sum;i+=2){
+             sum +=i;
+        }
+        return sum;
+    }
+}
