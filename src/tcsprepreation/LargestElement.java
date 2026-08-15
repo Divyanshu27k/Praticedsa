0dcbba02e4da304d2edc8b2f9c4600f9302983cc
Divyanshu Kumar <divyanshusingh0081@gmail.com>
2026-08-15 17:50:23 +0530

    tcs Question

---
+++ src/tcsprepreation/LargestElement.java
@@ -0,0 +1,22 @@
+package tcsprepreation;
+
+public class LargestElement {
+    public static void main(String[] args) {
+//        int arr[]={12,32,45,65,34};
+//        int max= arr[0];
+//        for (int i=1;i< arr.length;i++){
+//            if (arr[i]>max){
+//                max=arr[i];
+//            }
+//        }
+//        System.out.println(max);
+        int arr[]={1,2,3,4,5,6,7,8,9};
+        int max=arr[0];
+        for (int i=1;i< arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
