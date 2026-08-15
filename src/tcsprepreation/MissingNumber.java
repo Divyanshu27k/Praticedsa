867e966e440200a543be0e4f864c8b5433cc7d9c
Divyanshu Kumar <divyanshusingh0081@gmail.com>
2026-08-15 18:19:57 +0530

    tcs Question

---
+++ src/tcsprepreation/MissingNumber.java
@@ -0,0 +1,14 @@
+package tcsprepreation;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        int [] arr={1,2,3,5};
+        int n=5;
+        int expected =n*(n+1)/2;
+        int actual = 0;
+        for (int num : arr) {
+            actual += num;
+        }
+        System.out.println("Missing = " + (expected - actual));
+    }
+}
