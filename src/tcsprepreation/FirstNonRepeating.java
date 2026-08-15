3f799375e285910e6f9624772a7e3fe6b7568d65
Divyanshu Kumar <divyanshusingh0081@gmail.com>
2026-08-15 16:58:38 +0530

    tcs Question

--- src/tcsprepreation/FirstNonRepeating.java
+++ src/tcsprepreation/FirstNonRepeating.java
@@ -1,5 +1,21 @@
 package tcsprepreation;
 
-public class FirstNonRepeating {
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+public class FirstNonRepeating {
+    public static void main(String[] args) {
+        String str="swiss";
+        Map<Character,Integer> entry=new LinkedHashMap<>();
+         for(char ch : str.toCharArray()){
+             entry.put(ch,entry.getOrDefault(entry,0)+1);
+         }
+         for (Map.Entry<Character, Integer>entr : entry.entrySet()){
+             if (entr.getValue()==1){
+                 System.out.println(entr.getKey());
+                 break;
+             }
+         }
+    }
 }
