741f5cad4d99b28c3cb5292403a31c01459ac495
Divyanshu Kumar <divyanshusingh0081@gmail.com>
2026-08-15 17:10:33 +0530

    tcs Question

--- src/tcsprepreation/ReverseWords.java
+++ src/tcsprepreation/ReverseWords.java
@@ -3,10 +3,15 @@ package tcsprepreation;
 public class ReverseWords {
     public static void main(String[] args) {
 
-        String str="divyanshu kumar";
-
-        String[] words=str.split("");
-        for(int i= words.length-1;i>=0;--i){
+//        String str="divyanshu kumar";
+//
+//        String[] words=str.split("");
+//        for(int i= words.length-1;i>=0;--i){
+//            System.out.print(words[i]+"");
+//        }
+        String str="divyanshu";
+        String [] words=str.split("");
+        for (int i= words.length-1;i>=0;--i){
             System.out.print(words[i]+"");
         }
     }
