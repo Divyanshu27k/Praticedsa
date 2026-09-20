0fd57e92863864f5d09babf60f2f19d1a98f858d
Divyanshu Kumar <divyanshusingh0081@gmail.com>
2026-09-20 23:39:37 +0530

    interview practice

---
+++ src/interview/Ty.java
@@ -0,0 +1,12 @@
+package interview;
+
+import java.util.Scanner;
+
+public class Ty {
+    public static void main(String[] args) {
+        //Scanner sc =new Scanner(System.in);
+        int arr=987;
+       int reverse= new Integer(arr);
+        System.out.println(reverse);
+    }
+}
